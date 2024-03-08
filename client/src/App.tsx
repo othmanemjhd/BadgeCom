215121b12f23536a925cd961982af7905368b6d7
othmane moujahid <othmane.mjhd@gmail.com>
2024-03-08 17:01:15 +0100

    update on App.tsx - links

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,14 +6,15 @@ import Entree from "./pages/entree/Entree";
 import Sortie from "./pages/sortie/Sortie";
 import Add from "./pages/add/add";
 const App = () => {
+  const url = "https://badgecom.onrender.com";
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/entree" element={<Entree />}></Route>
-          <Route path="/add" element={<Add />}></Route>
-          <Route path="/sortie" element={<Sortie />}></Route>
+          <Route path={url + "/"} element={<Home />}></Route>
+          <Route path={url + "/entree"} element={<Entree />}></Route>
+          <Route path={url + "/add"} element={<Add />}></Route>
+          <Route path={url + "/sortie"} element={<Sortie />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
